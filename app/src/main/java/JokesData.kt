349fbfa5ecb14875c34a616db0627aebd06175f2
Arthur Delignette <arthur.delignette@edu.esiee.fr>
2022-03-21 18:54:01 +0100

    Recylcer view

---
+++ app/src/main/java/JokesData.kt
@@ -0,0 +1,3 @@
+data class JokesData(val text: String){
+
+}
--- app/src/main/java/com/example/chucknorrisproject/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisproject/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.chucknorrisproject
 
+import CustomAdapter
+import JokesData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<JokesData>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(JokesData("Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
     }
 }
